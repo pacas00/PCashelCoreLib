e3c1e55ad830261f1f68c06d0c291ed9be06b7ca
Peter Cashel <pacas00@gmail.com>
2015-11-08 19:00:34 +1000

    Create Constants file

---
+++ src/net/petercashel/commonlib/charset/CONSTANTS.java
@@ -0,0 +1,13 @@
+package net.petercashel.commonlib.charset;
+
+public final class CONSTANTS {
+
+	//ASCI 
+	public static String ASCI_RECORD_SEPARATOR = Character.toString((char)30);
+	public static String ASCI_UNIT_SEPARATOR = Character.toString((char)31);
+	
+	public static void Thing() {
+		
+	}
+	
+}
